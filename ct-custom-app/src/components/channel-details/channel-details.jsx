e24fe87ddd7ad69d1d7a46c360fda6896b6d85bb
dineshvadalacompose <dinesh@compose.co.in>
2026-05-14 14:53:59 +0530

    initial commit

---
+++ ct-custom-app/src/components/channel-details/channel-details.jsx
@@ -0,0 +1,148 @@
+import { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { useIntl } from 'react-intl';
+import { useParams } from 'react-router-dom';
+import {
+  useShowNotification,
+  useShowApiErrorNotification,
+} from '@commercetools-frontend/actions-global';
+import {
+  PageNotFound,
+  FormModalPage,
+} from '@commercetools-frontend/application-components';
+import { ApplicationPageTitle } from '@commercetools-frontend/application-shell';
+import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
+import { DOMAINS, NO_VALUE_FALLBACK } from '@commercetools-frontend/constants';
+import { formatLocalizedString } from '@commercetools-frontend/l10n';
+import { useIsAuthorized } from '@commercetools-frontend/permissions';
+import LoadingSpinner from '@commercetools-uikit/loading-spinner';
+import { ContentNotification } from '@commercetools-uikit/notifications';
+import Spacings from '@commercetools-uikit/spacings';
+import Text from '@commercetools-uikit/text';
+import { PERMISSIONS } from '../../constants';
+import {
+  useChannelDetailsUpdater,
+  useChannelDetailsFetcher,
+} from '../../hooks/use-channels-connector';
+import ChannelsDetailsForm from './channel-details-form';
+import { docToFormValues, formValuesToDoc } from './conversions';
+import messages from './messages';
+import { transformErrors } from './transform-errors';
+
+const ChannelDetails = (props) => {
+  const intl = useIntl();
+  const params = useParams();
+  const { loading, error, channel } = useChannelDetailsFetcher(params.id);
+  const { dataLocale, projectLanguages } = useApplicationContext((context) => ({
+    dataLocale: context.dataLocale ?? '',
+    projectLanguages: context.project?.languages ?? [],
+  }));
+  const canManage = useIsAuthorized({
+    demandedPermissions: [PERMISSIONS.Manage],
+  });
+  const showNotification = useShowNotification();
+  const showApiErrorNotification = useShowApiErrorNotification();
+  const channelDetailsUpdater = useChannelDetailsUpdater();
+  const handleSubmit = useCallback(
+    async (formikValues, formikHelpers) => {
+      const data = formValuesToDoc(formikValues);
+      try {
+        await channelDetailsUpdater.execute({
+          originalDraft: channel,
+          nextDraft: data,
+        });
+        showNotification({
+          kind: 'success',
+          domain: DOMAINS.SIDE,
+          text: intl.formatMessage(messages.channelUpdated, {
+            channelName: formatLocalizedString(formikValues, {
+              key: 'name',
+              locale: dataLocale,
+              fallbackOrder: projectLanguages,
+            }),
+          }),
+        });
+      } catch (error) {
+        const transformedErrors = transformErrors(error);
+        if (transformedErrors.unmappedErrors.length > 0) {
+          showApiErrorNotification({
+            errors: transformedErrors.unmappedErrors,
+          });
+        }
+
+        formikHelpers.setErrors(transformedErrors.formErrors);
+      }
+    },
+    [
+      channel,
+      channelDetailsUpdater,
+      dataLocale,
+      intl,
+      projectLanguages,
+      showApiErrorNotification,
+      showNotification,
+    ]
+  );
+
+  return (
+    <ChannelsDetailsForm
+      initialValues={docToFormValues(channel, projectLanguages)}
+      onSubmit={handleSubmit}
+      isReadOnly={!canManage}
+      dataLocale={dataLocale}
+    >
+      {(formProps) => {
+        const channelName = formatLocalizedString(
+          {
+            name: formProps.values?.name,
+          },
+          {
+            key: 'name',
+            locale: dataLocale,
+            fallbackOrder: projectLanguages,
+            fallback: NO_VALUE_FALLBACK,
+          }
+        );
+        return (
+          <FormModalPage
+            title={channelName}
+            isOpen
+            onClose={props.onClose}
+            isPrimaryButtonDisabled={
+              formProps.isSubmitting || !formProps.isDirty || !canManage
+            }
+            isSecondaryButtonDisabled={!formProps.isDirty}
+            onSecondaryButtonClick={formProps.handleReset}
+            onPrimaryButtonClick={formProps.submitForm}
+            labelPrimaryButton={FormModalPage.Intl.save}
+            labelSecondaryButton={FormModalPage.Intl.revert}
+          >
+            {loading && (
+              <Spacings.Stack alignItems="center">
+                <LoadingSpinner />
+              </Spacings.Stack>
+            )}
+            {error && (
+              <ContentNotification type="error">
+                <Text.Body>
+                  {intl.formatMessage(messages.channelDetailsErrorMessage)}
+                </Text.Body>
+              </ContentNotification>
+            )}
+            {channel && formProps.formElements}
+            {channel && (
+              <ApplicationPageTitle additionalParts={[channelName]} />
+            )}
+            {channel === null && <PageNotFound />}
+          </FormModalPage>
+        );
+      }}
+    </ChannelsDetailsForm>
+  );
+};
+ChannelDetails.displayName = 'ChannelDetails';
+ChannelDetails.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ChannelDetails;
---
+++ ct-custom-app/src/components/channel-details/validate.js
@@ -0,0 +1,19 @@
+import omitEmpty from 'omit-empty-es';
+import TextInput from '@commercetools-uikit/text-input';
+
+const validate = (formikValues) => {
+  const errors = {
+    key: {},
+    roles: {},
+  };
+
+  if (TextInput.isEmpty(formikValues.key)) {
+    errors.key.missing = true;
+  }
+  if (Array.isArray(formikValues.roles) && formikValues.roles.length === 0) {
+    errors.roles.missing = true;
+  }
+  return omitEmpty(errors);
+};
+
+export default validate;
